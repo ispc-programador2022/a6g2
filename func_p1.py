3dc5f0785554081ed0ef5eaa4aa9fcf7ff929194
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2022-06-11 01:50:36 -0300

    Creación del archivo y de la función necesaria

---
+++ func_p1.py
@@ -0,0 +1,31 @@
+# Simulación de funciones necesarias de otros archivos:
+# (Una vez creadas por los demás las importo)
+
+# Resta entre 2 parametros
+def resta(p1, p2):
+    return p1 - p2
+
+# Producto entre 2 parametros
+def producto(p1, p2):
+    return p1 * p2
+
+# --------------------------------------
+
+def p1(param1, param2, param3):
+    """Función que resta los dos primeros parámetros "p1" y "p2" y los
+       multipica por el parámetro "p3" usando las funciones externas "resta" y "producto".
+
+    Args:
+        p1 (float): Numero tipo float.
+        p2 (float): Numero tipo float.
+        p3 (float): Numero tipo float.
+
+    Returns:
+        float: Resultado final de las operaciones con redondeo a 2 decimales.
+    """
+    
+    op_resta = resta(param1, param2)
+    result = round(producto(op_resta, param3), 2)
+
+    return result
+
