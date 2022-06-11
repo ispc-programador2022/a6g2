f3e3374a4ef00e73307547df544e00475e1bf3da
Pedro Ghirardi <pedroghirardi49@gmail.com>
2022-06-11 20:39:20 -0300

    Agregar archivo funcion_suma

---
+++ funcion_suma.py
@@ -0,0 +1,10 @@
+
+#para1=hace referencia al parametro1
+#para2=hace referencia al parametro2
+#rtdo=hace referencia al resultado de la funcion suma 
+
+
+def suma(para1, para2):
+    global rtdo
+    rtdo= para1+para2
+    print(f"El resultado de la suma entre el parametro 1 y el parametro 2 es: {rtdo}")
