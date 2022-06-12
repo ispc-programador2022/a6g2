57ce5dd77931e05c7963e8009cf62c8f6f402f0e
Sebastian Galvan <sebastiangalvan.ok@gmail.com>
2022-06-12 19:07:57 -0300

    [ADD] Funcion producto

---
+++ producto.py
@@ -0,0 +1,11 @@
+def producto(n1, n2):
+    """Devuelve el producto entre dos parametros
+
+    Args:
+        n1 (int | float) 
+        n2 (int | float)
+
+    Returns:
+        (int | float)
+    """
+    return n1 * n2
