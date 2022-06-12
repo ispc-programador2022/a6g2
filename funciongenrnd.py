b7837dc3c529b887fd18dc5891f5aae90c10402a
fabiang78 <cursosfabiangomez@gmail.com>
2022-06-12 18:28:01 -0300

    agregar modificacion

--- funciongenrnd.py
+++ funciongenrnd.py
@@ -1,12 +1,12 @@
 
 import random as r
 def genrnd ():
-   
+    my_list= []
  
     for i in range (50):
         print( r.randint (0,100))
    
-    return 
+    return my_list
 #prueba
 #genrnd ()    
 
