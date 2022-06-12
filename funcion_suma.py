12135007c1db1ebca1acf0f5a4fb8820b30b0b33
pedroispc <106284729+pedroispc@users.noreply.github.com>
2022-06-12 20:35:13 -0300

    Update funcion_suma.py

--- funcion_suma.py
+++ funcion_suma.py
@@ -1,10 +1,15 @@
 
 #para1=hace referencia al parametro1
 #para2=hace referencia al parametro2
-#rtdo=hace referencia al resultado de la funcion suma 
+
 
 
 def suma(para1, para2):
-    global rtdo
-    rtdo= para1+para2
-    print(f"El resultado de la suma entre el parametro 1 y el parametro 2 es: {rtdo}")
+    """Devuelve el la suma entre dos parametros
+    Args:
+        n1 (int | float) 
+        n2 (int | float)
+    Returns:
+        (int | float)
+    """
+    return para1+para2
