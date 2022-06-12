2558b2d6b9d4aa3295341a603f021684e9a7ca0b
fabiang78 <cursosfabiangomez@gmail.com>
2022-06-12 20:22:30 -0300

    nueva modificacion

--- funciongenrnd.py
+++ funciongenrnd.py
@@ -4,11 +4,11 @@ def genrnd ():
     my_list= []
  
     for i in range (50):
-        print( r.randint (0,100))
+        my_list.append( r.randint (0,100))
    
     return my_list
-#prueba
-#genrnd ()    
+lista= genrnd()
+lista
 
 
  
