8d7ff7b22a465091dec000dbd9ca05d5782ec7f2
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2022-06-27 11:23:58 -0300

    Add: media_genrnd500k.py

---
+++ media_genrnd500k.py
@@ -0,0 +1,17 @@
+from genrnd500 import genrnd500
+
+def media_genrnd500k():
+    """Función que calcule la media del vector obtenido en genrnd500.
+
+    Returns:
+        int | float: Resultado final de la media del vector.
+    """
+        
+    num_list = genrnd500()
+    media = sum(num_list) / len(num_list)
+    
+    return media
+
+
+# Test
+# print(media_genrnd500k())
