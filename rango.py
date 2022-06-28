9911631da3bc03fc93a1e948361b5bdac1867fe1
Pedro Ghirardi <pedroghirardi49@gmail.com>
2022-06-27 21:19:18 -0300

    cambios en la funcion rango

--- rango.py
+++ rango.py
@@ -5,12 +5,12 @@ función genrnd que retorna una lista con 500.000.000.000.000.000 números aleat
 #mx es una variable que almacena el valor maximo de la lista
 #mn es una variable que almacena el valor minimo de la lista
 
-lista=[genrnd]
+lista=genrnd()
 
 def rango(lista):
     mx=max(lista)
     mn=min(lista)
     
+    
     return mx-mn
-
 
